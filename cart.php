ee0a2e5261cd32c092eec402b8e146e755b1fd99
RandomThirdy <angelitodecatoriaa@gmail.com>
2025-07-23 14:02:29 +0800

    Shows items added to the cart

--- cart.php
+++ cart.php
@@ -44,7 +44,6 @@ $grand_total = 0;
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Cart Section</title>
-
    <link rel="icon" type="images/png" href="images/sari-sari.png">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <link rel="stylesheet" href="css/style.css">
