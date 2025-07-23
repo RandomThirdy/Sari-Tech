f1a9c004f70f61c118cc6460268edf7c33b02b98
RandomThirdy <angelitodecatoriaa@gmail.com>
2025-07-23 14:02:49 +0800

    Lists products by category

--- category.php
+++ category.php
@@ -21,11 +21,9 @@ include 'components/add_cart.php';
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Category Section</title>
-
    <link rel="icon" type="images/png" href="images/sari-sari.png">
    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
    <link rel="stylesheet" href="css/style.css">
 
 </head>
